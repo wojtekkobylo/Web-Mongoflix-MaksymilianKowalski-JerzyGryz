b843b3986955a8264e8a3f762dbd9754c2ad3706
jurekg <jurekgz27@gmail.com>
2026-02-24 14:25:04 +0100

    Stworzone bazę danych filmy i testowano dodawanie filmów do kolekcji

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,1 +1,19 @@
-//
+db = db.getSiblingDB('filmyDB');
+
+db.filmy.insertMany([
+        {
+            "tytul": "Minionki",
+            "premiera": 2015,
+            "gatunek": ["Komedia", "Familijny"]
+        },
+        {
+            "tytul": "Incepcja",
+            "premiera": 2010,
+            "gatunek": ["Sci-Fi", "Thriller"]
+        },
+        {
+            "tytul": "Shrek",
+            "premiera": 2001,
+            "gatunek": ["Animacja", "Komedia", "Przygodowy"]
+        }
+]);
